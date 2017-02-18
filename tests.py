e61b0ae0fa3a73f3ff2c17931f5fd7eea6e546bc
Duncan Macleod <duncan.macleod@ligo.org>
2017-02-17 18:48:03 -0600

    travis-ci: added null test suite

---
+++ tests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) Duncan Macleod (2013)
+#
+# This file is part of LIGO.ORG.
+#
+# LIGO.ORG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# LIGO.ORG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LIGO.ORG.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from ligo.org import request
+
+
+class LigoDotOrgTests(unittest.TestCase):
+    def test_null(self):
+        pass
