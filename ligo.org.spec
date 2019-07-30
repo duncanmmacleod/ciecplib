ce7795fac17d0a0db4d863168c0861ef2afbda11
Duncan Macleod <duncan.macleod@ligo.org>
2019-07-30 16:51:41 +0100

    rebuild of packaging and test suite

---
+++ ligo.org.spec
@@ -0,0 +1,97 @@
+%define origname ligo.org
+%define pkgname ligo-org
+%define version 0.1.0
+%define release 1
+
+# -- metadata ---------------
+
+BuildArch: noarch
+Group:     Development/Libraries
+License:   GPL-3.0-or-later
+Name:      %{pkgname}
+Packager:  Duncan Macleod <duncan.macleod@ligo.org>
+Prefix:    %{_prefix}
+Release:   %{release}%{?dist}
+Source0:   https://pypi.io/packages/source/l/%{origname}/%{origname}-%{version}.tar.gz
+Summary:   A python client for LIGO.ORG SAML ECP authentication
+Url:       https://github.com/duncanmmacleod/ligo.org
+Vendor:    LIGO Scientific Collaboration <auth@ligo.org>
+Version:   %{version}
+
+# -- build requirements -----
+
+BuildRequires: help2man
+BuildRequires: python
+BuildRequires: python-rpm-macros
+BuildRequires: python2-rpm-macros
+BuildRequires: python2-setuptools
+
+# -- packages ---------------
+
+# src.rpm
+%description
+The Python client for LIGO.ORG SAML ECP authentication.
+
+%package -n python2-%{pkgname}
+Summary: %{summary}
+Requires: ligo-ca-certs
+Requires: m2crypto
+Requires: osg-ca-certificates
+Requires: pyOpenSSL
+Requires: python
+Requires: python-lxml
+Requires: python-kerberos
+Requires: python2-ligo-common
+%{?python_provide:%python_provide python2-%{pkgname}}
+%description -n python2-%{pkgname}
+The Python %{python_version} client for LIGO.ORG SAML ECP authentication.
+
+%package -n ligo-proxy-utils2
+Summary: Command line utilities for LIGO.ORG SAML ECP authentication
+Requires: python2-%{pkgname}
+Conflicts: ligo-proxy-utils
+%description -n ligo-proxy-utils2
+Command line utilities for LIGO.ORG SAML ECP authentication, including
+ligo-proxy-init and ligo-curl (a LIGO.ORG-aware curl alternative).
+
+# -- build ------------------
+
+%prep
+%autosetup -n %{origname}-%{version}
+
+%build
+%py2_build
+
+%install
+%py2_install
+# make man pages
+mkdir -vp %{buildroot}%{_mandir}/man1
+ls %{buildroot}%{_bindir} | \
+env PYTHONPATH="%{buildroot}%{python2_sitelib}" \
+xargs --verbose -I @ \
+help2man \
+    --source %{origname} \
+    --version-string %{version} \
+    --section 1 \
+    --no-info \
+    --no-discard-stderr \
+    --output %{buildroot}%{_mandir}/man1/@.1 \
+    %{buildroot}%{_bindir}/@
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+# -- files ------------------
+
+%files -n python2-%{pkgname}
+%license LICENSE
+%doc README.md
+%{python2_sitelib}/*
+
+%files -n ligo-proxy-utils2
+%license LICENSE
+%{_bindir}/ligo-*
+%{_mandir}/man1/ligo-*.1*
+
+
+# -- changelog --------------
---
+++ ligo/org/tests/test_utils.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Duncan Macleod (2019)
+#
+# This file is part of LIGO.ORG.
+#
+# LIGO.ORG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# LIGO.ORG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LIGO.ORG.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests for :mod:`ligo.org.utils`
+"""
+
+try:
+    from unittest import mock
+except ImportError:  # python < 3
+    import mock
+    urlreqmodname = "urllib2"
+else:
+    urlreqmodname = "urllib.request"
+
+import pytest
+
+from .. import utils as ligodotorg_utils
+
+__author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
+
+RAW_INSTITUTION_LIST = [
+    b"https://inst1.test Institution 1",
+    b"https://inst2.test Institution 2",
+]
+
+
+@mock.patch(
+    "{}.urlopen".format(urlreqmodname),
+    return_value=RAW_INSTITUTION_LIST,
+)
+def test_get_idps(urlopen):
+    assert ligodotorg_utils.get_idps("something") == {
+        "Institution 1": "https://inst1.test",
+        "Institution 2": "https://inst2.test",
+    }
+    assert urlopen.called_with("something")
+
+
+@mock.patch(
+    "{}.urlopen".format(urlreqmodname),
+    return_value=RAW_INSTITUTION_LIST,
+)
+def test_get_idp_urls(_):
+    assert ligodotorg_utils.get_idp_urls("Institution 1") == (
+        "https://inst1.test",
+        "https://inst1.test",
+    )
+
+
+@mock.patch(
+    "{}.urlopen".format(urlreqmodname),
+    return_value=RAW_INSTITUTION_LIST,
+)
+@pytest.mark.parametrize("inst", ["Institution*", "something else"])
+def test_get_idp_urls_error(_, inst):
+    with pytest.raises(ValueError):
+        ligodotorg_utils.get_idp_urls(inst)
+
+
+@pytest.mark.parametrize("url, result", [
+    ("login.ligo.org", "https://login.ligo.org/idp/profile/SAML2/SOAP/ECP"),
+    ("login.ligo.org/test", "https://login.ligo.org/test"),
+    ("https://login.ligo.org/idp/profile/SAML2/SOAP/ECP",
+     "https://login.ligo.org/idp/profile/SAML2/SOAP/ECP"),
+])
+def test_format_endpoint_url(url, result):
+    assert ligodotorg_utils.format_endpoint_url(url) == result
