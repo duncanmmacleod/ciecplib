dff96897bacc1aaec8b3073b1f73bf583666d2d9
Duncan Macleod <duncan.macleod@ligo.org>
2019-08-15 12:16:05 +0100

    ligo.org: fixed python2 compatibility issues

--- ligo/org/cookies.py
+++ ligo/org/cookies.py
@@ -19,6 +19,8 @@
 """Cookie handling for LIGO.ORG SAML ECP authentication
 """
 
+from __future__ import print_function
+
 import os
 import time
 import warnings
--- ligo/org/requests.py
+++ ligo/org/requests.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with LIGO.ORG.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import (print_function, absolute_import)
+from __future__ import absolute_import
 
 try:
     from urllib.request import Request
